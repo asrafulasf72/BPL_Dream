e5eabaff8e77bb6c7eccbb597d14403278b6d37e
asraful <asrafulislamasf@gmail.com>
2025-09-24 11:38:55 +0600

    Conditional Randering Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import AvailablePlayers from './Componant/AvailablePlayers'
 import Banner from './Componant/Banner'
@@ -13,15 +13,27 @@ const fetchPlayers =async()=>{
 }
 
 function App() {
+
+  const [toggle, setToggle]=useState(false)
   const promisePlayers=fetchPlayers();
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+       <div className='max-w-[1200px] mx-auto flex justify-between items-center mb-5'>
+           <h1 className='text-[1.5rem] font-medium'>Available Players</h1>
+           <div className='font-semibold'>
+                <button onClick={()=> setToggle(true)} className={`py-3 px-4 border-1 border-gray-300 rounded-l-[8px] cursor-pointer border-r-0 ${toggle===true?'bg-yellow-300': ""}`}>Available</button>
+                <button onClick={()=> setToggle(false)} className={`py-3 px-4 border-1 border-gray-300 rounded-r-[8px] cursor-pointer border-l-0 ${toggle===false?'bg-yellow-300': ""}`}>Selected <span>(0)</span></button>
+           </div>
+       </div>
+       {
+        toggle=== true?  <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
         <AvailablePlayers promisePlayers={promisePlayers}></AvailablePlayers>
-      </Suspense>
-      {/* <SelecetedPlayers></SelecetedPlayers> */}
+      </Suspense> : <SelecetedPlayers></SelecetedPlayers>
+       }
+     
+      
     </>
   )
 }
--- src/Componant/SelecetedPlayers.jsx
+++ src/Componant/SelecetedPlayers.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SelecetedPlayers = () => {
     return (
-        <div>
-            <h1>Seleceted Players</h1>
+        <div className='max-w-[1200px] mx-auto'>
+            <h1 className='text-[1.5rem] font-medium'>Seleceted Players</h1>
         </div>
     );
 };
